a2b734505c59b45c9f0f67299b31041bef384af4
tasmina@hotmail.com <tasmina@hotmail.com>
2026-01-20 14:07:19 +0100

    backend and api lager completed and small minor changes to constants file

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,9 +1,19 @@
 from pathlib import Path 
 
-DATA_PATH = Path(__file__).parents[1] / "data"
+BASE_PATH = Path(__file__).parents[1]  #src/taxipred
+
+DATA_PATH = BASE_PATH / "data"
+MODEL_PATH = BASE_PATH / "model_development"
+
+
+
+TAXI_CLEANED_CSV = MODEL_PATH / "taxi_cleaned_training_data.csv"
+JOBLIB_PATH = MODEL_PATH / "LM_model.joblib"
+
+# DATA_PATH = Path(__file__).parents[1] / "data"
+# MODEL_PATH = Path(__file__).parent / "model_development"
+
 
 TAXI_CSV_PATH = DATA_PATH / "taxi_trip_pricing.csv"
-
-MODEL_PATH = Path(__file__).parent / "model_development"
-
-JOBLIB_PATH = MODEL_PATH/ "LM_model.joblib"
+#TAXI_CLEANED_CSV = MODEL_PATH/ "taxi_cleaned_training_data.csv"
+#OBLIB_PATH = MODEL_PATH/ "LM_model.joblib"
